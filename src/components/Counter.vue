c142e3ba3e6373acf7c070225bc30cee58feb72d
Deividi-Lavitz <deividi.luvison@gmail.com>
2023-09-21 01:35:22 -0300

    3 files modified, 1 added, 2 deleted

---
+++ src/components/Counter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function incrementCounter() {
+  count.value++;
+}
+
+function onClick() {
+  incrementCounter();
+}
+</script>
+<template>
+  <div>
+    <button @click="onClick">Click Here!</button>
+    <span>Was clicked {{ count }} times!</span>
+  </div>
+</template>
